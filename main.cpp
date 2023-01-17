925ea132ee02b82868d9c3380c36dbbfac11c38e
Joshua Park <sp72@calvin.edu>
2023-01-17 12:51:14 -0500

    lab 1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
+#include <iostream>
+#include <string>
+#include <cassert>
+
+using namespace std;
 
 int main() {
+
+    unsigned value[8];
+
+    for (int i = 0; i < 8 ; i++)
+    {
+    value[i] = i * 7;
+    }
+
+    assert(value[0] == 0);
+    assert(value[3] == 21);
+    assert(value[7] == 49);
+
+    cout << "All tests passed!"<< endl;
 
     return 0;
 }
